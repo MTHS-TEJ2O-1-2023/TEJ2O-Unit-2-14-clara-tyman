370d97f6d988470af59a0d0dea70b316687520be
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-11-07 12:47:43 -0500

    ts version

--- main.ts
+++ main.ts
@@ -19,12 +19,14 @@ input.onButtonPressed(Button.A, function () {
   basic.clearScreen()
   loopCounter = 0
   sprite = game.createSprite(0, 0)
+
   while (loopCounter <= 5) {
     basic.pause(500)
     sprite.set(LedSpriteProperty.X, loopCounter)
     sprite.set(LedSpriteProperty.Y, loopCounter)
     loopCounter = loopCounter + 1
   }
+
   sprite.delete()
   basic.showIcon(IconNames.Happy)
 })
@@ -34,12 +36,14 @@ input.onButtonPressed(Button.B, function () {
   basic.clearScreen()
   loopCounter = 5
   sprite = game.createSprite(5, 5)
+
   while (loopCounter >= -1) {
     basic.pause(500)
     sprite.set(LedSpriteProperty.X, loopCounter)
     sprite.set(LedSpriteProperty.Y, loopCounter)
     loopCounter = loopCounter - 1
   }
+
   sprite.delete()
   basic.showIcon(IconNames.Happy)
 })
