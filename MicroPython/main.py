3c4a107f0be07aa3ae334f3b07140a0b2cd660aa
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-11-07 18:29:57 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,47 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Clara
+Created on: Nov 2023
+This module is a Micro:bit MicroPython program moves LEDs on the Micro:bit diagonally.
 """
 
 from microbit import *
 
+# variables
+set_pixel = None
+loopCounter = 0
 
-display.scroll("Hello, World!")
+# Setup
+display.show(Image.SILLY)
+display.clear()
+sleep(500)
+
+# Press button A
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        loopCounter = 0
+        display.set_pixel (x, y.value)
+
+        while loopCounter <= 5:
+            sleep(500)
+            set_pixel(LEDSpriteProperty.X, loopCounter)
+            set_pixel(LEDSpriteProperty.Y, loopCounter)
+            loopCounter = loopCounter + 1
+
+        set_pixel.delete()
+        display.show(Image.SILLY)
+
+        # Press button B
+        if button_b.is_pressed():
+            display.clear()
+            loopCounter = 5
+            display.set_pixel (x, y.value)
+
+        while loopCounter >= -1:
+            sleep(500)
+            set_pixel(LEDSpriteProperty.X, loopCounter)
+            set_pixel(LEDSpriteProperty.Y, loopCounter)
+            loopCounter = loopCounter - 1
+
+        set_pixel.delete()
+        display.show(Image.SILLY)
